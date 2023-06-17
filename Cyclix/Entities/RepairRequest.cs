c75d7ed35ea8a415f4bb1805520b10792d5e5e30
BhuiyanSaif18 <saiful.bhuiyan@planally.com>
2023-06-18 00:44:35 +0600

    Entity and migration added

---
+++ Cyclix/Entities/RepairRequest.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cyclix.Entities
+{
+    public class RepairRequest
+    {
+        [Key]
+        public int id{ get; set; }
+        public string cycleType { get; set; }
+        public string brandName { get; set; }
+
+
+        public string servicePricingOption { get; set; }
+        public bool skipServicePackage { get; set; }
+        public bool isElectricCycle { get; set; }
+
+
+        public bool tubeOrTire { get; set; }
+        public bool wheelCentering { get; set; }
+        public bool adjustingGears { get; set; }
+        public bool adjectBreaks { get; set; }
+        public bool changeBreakPads { get; set; }
+        public bool suspension { get; set; }
+        public bool completeCleaning { get; set; }
+        public bool lightCheck { get; set; }
+        public bool functionCheck { get; set; }
+        public bool batteryCheck { get; set; }
+
+
+        public string moreDetailedIssue { get; set; }
+        public double estimatedCost { get; set; }
+
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string street { get; set; }
+        public string nr { get; set; }
+        public string city { get; set; }
+        public string zipCode { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ Cyclix/Migrations/20230617165906_Repair-Requests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cyclix.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepairRequests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RepairRequests",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    cycleType = table.Column<string>(type: "TEXT", nullable: false),
+                    brandName = table.Column<string>(type: "TEXT", nullable: false),
+                    servicePricingOption = table.Column<string>(type: "TEXT", nullable: false),
+                    skipServicePackage = table.Column<bool>(type: "INTEGER", nullable: false),
+                    isElectricCycle = table.Column<bool>(type: "INTEGER", nullable: false),
+                    tubeOrTire = table.Column<bool>(type: "INTEGER", nullable: false),
+                    wheelCentering = table.Column<bool>(type: "INTEGER", nullable: false),
+                    adjustingGears = table.Column<bool>(type: "INTEGER", nullable: false),
+                    adjectBreaks = table.Column<bool>(type: "INTEGER", nullable: false),
+                    changeBreakPads = table.Column<bool>(type: "INTEGER", nullable: false),
+                    suspension = table.Column<bool>(type: "INTEGER", nullable: false),
+                    completeCleaning = table.Column<bool>(type: "INTEGER", nullable: false),
+                    lightCheck = table.Column<bool>(type: "INTEGER", nullable: false),
+                    functionCheck = table.Column<bool>(type: "INTEGER", nullable: false),
+                    batteryCheck = table.Column<bool>(type: "INTEGER", nullable: false),
+                    moreDetailedIssue = table.Column<string>(type: "TEXT", nullable: false),
+                    estimatedCost = table.Column<double>(type: "REAL", nullable: false),
+                    firstName = table.Column<string>(type: "TEXT", nullable: false),
+                    lastName = table.Column<string>(type: "TEXT", nullable: false),
+                    street = table.Column<string>(type: "TEXT", nullable: false),
+                    nr = table.Column<string>(type: "TEXT", nullable: false),
+                    city = table.Column<string>(type: "TEXT", nullable: false),
+                    zipCode = table.Column<string>(type: "TEXT", nullable: false),
+                    email = table.Column<string>(type: "TEXT", nullable: false),
+                    phone = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepairRequests", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RepairRequests");
+        }
+    }
+}
